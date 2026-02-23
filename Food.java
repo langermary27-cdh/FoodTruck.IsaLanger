efd77d77c3a2398cc58043fb61482a109e30f5b7
langermary27-cdh <langer.mary.27@c-dh.org>
2026-02-23 12:22:08 -0600

    Create Food.java

---
+++ Food.java
@@ -0,0 +1,16 @@
+public class Food 
+{
+    private int calories;
+    private String foodName;
+    private Double price;
+
+    public static void main(String[] args) 
+    {
+    }
+    public Food(String foodName, int calories, double price)
+    {
+        this.foodName = foodName;
+        this.calories = calories;
+        this.price = price;
+    }
+}
